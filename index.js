ce4b8cc7b9250403ff2aabbcc38631278d479e06
Elizabeth Carolina <celerner4@gmail.com>
2021-08-27 14:58:56 -0300

    Agrego menú de inicio, falta corregir arrow

--- index.js
+++ index.js
@@ -23,9 +23,7 @@ function startTime(){
 today = new Date();
 h = today.getHours();
 m = today.getMinutes();
-s = today.getSeconds();
 m = checkTime(m);
-s = checkTime(s);
 document.getElementById('clock').innerHTML= h + ":" + m ;
 t = setTimeout('startTime()',500);}
 function checkTime(i)
