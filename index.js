58fbc43f78dde05629dd40beeec5b9bcd4fa9b63
Elizabeth Carolina <celerner4@gmail.com>
2021-08-26 21:53:48 -0300

    classlist.toggle en boton start y cambio de shadow en el boton

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log("hola")
+
+// BUTTON START 
+
+const buttonStart = document.getElementById ("button-start")
+console.log(buttonStart)
+
+const startClass = document.querySelector (".start")
+console.log(startClass)
+const startClassToggle = document.querySelector (".start-toggle")
+console.log(startClassToggle)
+
+const body = document.getElementById("body")
+
+buttonStart.addEventListener("click" , () =>{
+buttonStart.classList.toggle("start")
+buttonStart.classList.toggle("start-toggle")
+}) 
+
+
